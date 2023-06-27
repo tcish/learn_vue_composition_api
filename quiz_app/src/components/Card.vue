26336d319d104ba775c413d643f94d3d3416bb33
tcish <khaladkhan250@gmail.com>
2023-06-27 14:27:38 +0600

    added vue router and single quiz view page

--- quiz_app/src/components/Card.vue
+++ quiz_app/src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <div class="card" @click="navigateToQuiz">
     <img :src="quiz.img" alt="" />
     <div class="card-text">
       <h2>{{ quiz.name }}</h2>
@@ -8,10 +8,16 @@
   </div>
 </template>
 
-<script setup> 
+<script setup>
 import { defineProps } from "vue";
+import { useRouter } from "vue-router";
 
+const router = useRouter();
 const { quiz } = defineProps(["quiz"]);
+
+const navigateToQuiz = () => {
+  router.push(`quiz/${quiz.id}`);
+};
 </script>
 
 
